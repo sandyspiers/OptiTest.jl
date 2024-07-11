ae862b040173ab6cf365fa99151e3d75399c9e3c
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-11 16:24:27 +0800

    recursive setter

--- src/utils.jl
+++ src/utils.jl
@@ -13,14 +13,30 @@ function all_keys(dict::AbstractDict, parent_key::Tuple=())::Set{Tuple}
     return key_list
 end
 
+function rec_set!(dict::AbstractDict, key, val)::AbstractDict
+    if key ∈ keys(dict)
+        dict[key] = val
+    elseif typeof(key) <: Tuple
+        if first(key) ∈ keys(dict)
+            sub_dict = dict[first(key)]
+            if typeof(sub_dict) <: AbstractDict
+                rec_set!(sub_dict, key[2:end], val)
+            else
+                dict[first(key)] = val
+            end
+        end
+    end
+    return dict
+end
+
 function rec_get(dict::AbstractDict, key, fallback=nothing)
     if typeof(key) <: Tuple
         if first(key) ∈ keys(dict)
-            val = dict[first(key)]
-            if typeof(val) <: AbstractDict
-                return rec_get(val, key[2:end], fallback)
+            sub_dict = dict[first(key)]
+            if typeof(sub_dict) <: AbstractDict
+                return rec_get(sub_dict, key[2:end], fallback)
             else
-                return val
+                return sub_dict
             end
         end
     end
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -12,4 +12,9 @@
     # test rec get
     d = Dict("x" => nothing, "y" => Dict("z" => 5))
     @test rec_get(d, ("y", "z")) == 5
+
+    # test rec set
+    d = Dict("x" => nothing, "y" => Dict("z" => 5))
+    rec_set!(d, ("y", "z"), 100)
+    @test d["y"]["z"] == 100
 end
