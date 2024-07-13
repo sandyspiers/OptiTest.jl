18f02baa7363adddd534182f888d47337b45cd43
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-13 13:15:17 +0800

    improved utils functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,57 +1,96 @@
-# # helper function to get all keys in a multilevel dict
-function all_keys(dict, key_list=Tuple[], parent_key=())::Vector{Tuple}
-    if typeof(dict) <: AbstractDict
-        for (key, sub_dict) in dict
-            all_keys(sub_dict, key_list, (parent_key..., key))
-        end
-    else
-        push!(key_list, parent_key)
-    end
-    return key_list
+"""
+    all_but_last(iterable)
+
+Takes an iterable and returns the set [first,last) element.
+I.e., if the iterble is only of lenth one, it will still return
+the first element
+"""
+function all_but_last(iterable)
+    return iterable[1:max(1, end - 1)]
 end
 
-function expand(tup)
-    if length(tup) > 1
-        return (tup[1:(end - 1)], tup[end]...)
-    end
-    return tup
-end
+"""
+    nested_keys(dict)
 
-function tup_keys(dict, parent=())
+Takes a potentially nested dictionary, and returns a vector of tuples
+giving the nested keys.
+Warning: Will fail if some of the dictionaries keys are already tuples.
+
+```julia
+julia> d = Dict("aa" => 1, "bb" => Dict("xx" => 2, "yy" => Dict("gg"=>3,"hh"=>4)))
+Dict{String, Any} with 2 entries:
+  "bb" => Dict{String, Any}("xx"=>2, "yy"=>Dict("hh"=>4, "gg"=>3))
+  "aa" => 1
+
+julia> nested_keys(d)
+4-element Vector{Tuple{String, Vararg{String}}}:
+ ("bb", "xx")
+ ("bb", "yy", "hh")
+ ("bb", "yy", "gg")
+ ("aa",)
+```
+"""
+nested_keys(dict) = _nested_keys(dict, ())
+function _nested_keys(dict, parent)
     if typeof(dict) <: AbstractDict
-        ret = collect(tup_keys(sd, (parent..., sk)) for (sk, sd) in dict)
-        @info "ret" ret
-        return ret
+        sub_keys = (_nested_keys(sd, (parent..., sk)) for (sk, sd) in dict)
+        return typeof(sub_keys) <: Tuple ? sub_keys : vcat(sub_keys...)
     end
     return (parent)
 end
 
-function rec_set!(dict::AbstractDict, key, val)::AbstractDict
+"""
+    nested_get(dict::AbstractDict, key, fallback=nothing)
+
+Gets the element inside a potentially nested dictionary, where `key`
+defines the tuple-key.
+
+```julia
+julia> d = Dict("aa" => 1, "bb" => Dict("xx" => 2, "yy" => Dict("gg"=>3,"hh"=>4)))
+Dict{String, Any} with 2 entries:
+  "bb" => Dict{String, Any}("xx"=>2, "yy"=>Dict("hh"=>4, "gg"=>3))
+  "aa" => 1
+
+julia> nested_get(d,("bb","yy"))
+Dict{String, Int64} with 2 entries:
+  "hh" => 4
+  "gg" => 3
+
+julia> nested_get(d,"aa")
+1
+```
+"""
+function nested_get(dict::AbstractDict, key, fallback=nothing)
     if key ∈ keys(dict)
-        dict[key] = val
-    elseif typeof(key) <: Tuple
-        if first(key) ∈ keys(dict)
-            sub_dict = dict[first(key)]
-            if (typeof(sub_dict) <: AbstractDict) & (length(key) > 1)
-                rec_set!(sub_dict, key[2:end], val)
-            else
-                dict[first(key)] = val
-            end
-        end
+        return get(dict, key, fallback)
     end
-    return dict
+    return reduce((dict, key) -> get(dict, key, fallback), key; init=dict)
 end
 
-function rec_get(dict::AbstractDict, key, fallback=nothing)
-    if typeof(key) <: Tuple
-        if first(key) ∈ keys(dict)
-            sub_dict = dict[first(key)]
-            if typeof(sub_dict) <: AbstractDict
-                return rec_get(sub_dict, key[2:end], fallback)
-            else
-                return sub_dict
-            end
-        end
+"""
+    nested_set!(dict::AbstractDict, key, val)::AbstractDict
+
+
+Sets the element inside a potentially nested dictionary, where `key`
+defines the tuple-key.
+```julia
+julia> d = Dict("aa" => 1, "bb" => Dict("xx" => 2, "yy" => Dict("gg"=>3,"hh"=>4)))
+Dict{String, Any} with 2 entries:
+  "bb" => Dict{String, Any}("xx"=>2, "yy"=>Dict("hh"=>4, "gg"=>3))
+  "aa" => 1
+
+julia> nested_set!(d,("bb","xx"),6)
+Dict{String, Any} with 2 entries:
+  "bb" => Dict{String, Any}("xx"=>6, "yy"=>Dict("hh"=>4, "gg"=>3))
+  "aa" => 1
+```
+"""
+function nested_set!(dict::AbstractDict, key, val)::AbstractDict
+    if key ∈ keys(dict)
+        dict[key] = val
+    else
+        sub_dict = nested_get(dict, all_but_last(key))
+        sub_dict[last(key)] = val
     end
-    return get(dict, key, fallback)
+    return dict
 end
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,16 +1,19 @@
 @testset "utils.jl" begin
     # test all keys method
     d = Dict("x" => nothing, "y" => 5)
-    @test all_keys(d) == [("x",), ("y",)]
-    d = Dict("x" => nothing, "y" => Dict("z" => 5))
-    @test all_keys(d) == [("x",), ("y", "z")]
+    @test nested_keys(d) == [("x",), ("y",)]
+    d = Dict("xx" => nothing, "yy" => Dict("zz" => 5), "aa" => "hello")
+    @test nested_keys(d) == [("xx",), ("yy", "zz"), ("aa",)]
 
-    # test rec get
+    # test nested get
     d = Dict("x" => nothing, "y" => Dict("z" => 5))
-    @test rec_get(d, ("y", "z")) == 5
+    @test nested_get(d, ("y", "z")) == 5
+    @test nested_get(d, "x") === nothing
 
-    # test rec set
-    d = Dict("x" => nothing, "y" => Dict("z" => 5))
-    rec_set!(d, ("y", "z"), 100)
-    @test d["y"]["z"] == 100
+    # test nested set
+    d = AnyDict("x" => nothing, "y" => AnyDict("z" => 5))
+    nested_set!(d, ("y", "z"), "hello!")
+    @test d["y"]["z"] == "hello!"
+    nested_set!(d, "x", 50)
+    @test d["x"] == 50
 end
