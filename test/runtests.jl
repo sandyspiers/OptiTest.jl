172f00dcb853d02073e1078b221bc42ca1ebef3e
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-16 11:29:33 +0800

    remove aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,7 @@ using OptiTest: OptiTest, Experiment
 using OptiTest: iterated_dict!
 using OptiTest: tests!, tests, run
 using Test
-using Aqua
 
 include("test_utils.jl")
 include("test_experiment.jl")
 include("test_distributed.jl")
-
-# @testset "Code quality (Aqua.jl)" begin
-#     Aqua.test_all(OptiTest)
-# end
