fcd2ac0a8a3bc6d9b742dfde766a0f14f9739e12
sandyspiers <sandy.spiers@curtin.edu.au>
2024-07-19 14:57:17 +0800

    working style guide

--- example.jl
+++ example.jl
@@ -1,4 +1,3 @@
-using Base: length_continued
 using DataFrames
 using Plots
 import Plots: plot, plot!
@@ -11,13 +10,13 @@ abstract type PlotData end
 struct PerformanceProfile <: PlotData
     max_time::Real
     num_tests::Integer
-    labels::Vector{AbstractDict}
+    labels::Vector{Vector{Pair}}
     solve_times::Vector{Vector{Real}}
     # some attributes
     function PerformanceProfile(df, identifiers, solve_time)
         max_time = maximum(df[!, solve_time])
         num_tests = 0
-        labels = AbstractDict[]
+        labels = Vector[]
         solve_times = Vector[]
         # for each group
         for g in groupby(df, identifiers)
@@ -33,7 +32,7 @@ struct PerformanceProfile <: PlotData
             # save labels as dictionary
             push!(
                 labels,
-                Dict(identifier => first(g[!, identifier]) for identifier in identifiers),
+                [(identifier => first(g[!, identifier])) for identifier in identifiers],
             )
         end
         return new(max_time, num_tests, labels, solve_times)
@@ -42,12 +41,43 @@ end
 function plot!(pp::PerformanceProfile, style_guide::AbstractDict)
     for (label, times) in zip(pp.labels, pp.solve_times)
         steps = vcat(0:(length(times) - 2), length(times) - 1)
-        plot!(steps, times; label="$label", seriestype=:steppost)
+        @info style_kwargs(label, style_guide)
+        plot!(
+            steps,
+            times;
+            label="$label",
+            seriestype=:steppost,
+            style_kwargs(label, style_guide)...,
+        )
     end
     return nothing
 end
 
-function style_kwargs(plot_data::PlotData)::Dict
+function style_kwargs(labels::Vector{Pair}, style_guide::AbstractDict)::AbstractDict
+    return style_kwargs(labels, style_guide, Dict(), 0)
+end
+function style_kwargs(labels::Vector{Pair}, style_guide::AbstractDict, depths, depth)
+    style = Dict()
+    for (k, v) in style_guide
+        if isa(k, Pair) && isa(v, AbstractDict)
+            if k ∈ labels
+                for (kk, vv) in style_kwargs(labels, v, depths, depth + 1)
+                    _update_style_depths!(kk, vv, style, depths, depth)
+                end
+            end
+        else
+            _update_style_depths!(k, v, style, depths, depth)
+        end
+    end
+    return style
+end
+function _update_style_depths!(key, val, style, depths, depth)
+    if key ∉ keys(depths)
+        depths[key] = depth
+    end
+    if key ∉ keys(style) || depths[key] <= depth
+        style[key] = val
+    end
     return nothing
 end
 
@@ -81,4 +111,15 @@ experiment = Dict(
 df = run(experiment, random_solve_time)
 
 pp = PerformanceProfile(df, [:speed, :motivation], :solve_time)
-plot(pp)
+sg = Dict(
+    (:speed => :slow) => Dict("color" => "brown", "marker" => "!"),
+    (:speed => :medium) => Dict("marker" => "!"),
+    (:speed => :fast) => Dict("color" => "red", "marker" => "!"),
+    (:motivation => :low) => Dict((:speed => :slow) => Dict("marker" => "*")),
+)
+sg = Dict(
+    (:speed => :slow) => Dict(:label => "snail"),
+    (:speed => :fast) => Dict(:label => "sanic"),
+)
+println(pp.labels)
+plot(pp, sg)
