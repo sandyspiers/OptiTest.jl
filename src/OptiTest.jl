1b000767bfa9fc456b5a2df06a4e02a9d07259c6
sandyspiers <sandy.spiers@curtin.edu.au>
2024-08-06 17:20:54 +0800

    commiting this nonsense

--- src/OptiTest.jl
+++ src/OptiTest.jl
@@ -2,25 +2,21 @@ module OptiTest
 
 import Plots: plot, plot!, hline!
 
-using Base.Iterators: product
-using CSV: write
-using DataFrames: DataFrame, groupby
-using Dates: format, now
-using Distributed: pmap
-using JSON: print
-
 # # generic utility functions, include dictionary producting
 include("utils.jl")
+
+# # special struct stype
+include("masked_named_tuple.jl")
 
 # # experimental functions, including special functions and keywords
 # # special names and sane defaults
 include("experiment.jl")
 
 # # the generic semi-abstract procedure for producing plots
-include("ploter.jl")
+# include("ploter.jl")
 
 # # a list of predefined plots and performance profiles
-include("plots.jl")
-const PLOT_TYPES = [PerformanceProfile]
+# include("plots.jl")
+# const PLOT_TYPES = [PerformanceProfile]
 
 end
--- src/experiment.jl
+++ src/experiment.jl
@@ -1,75 +1,74 @@
-# # some sane defaults
-key!(key) = last(key) == '!'
-rm!(key) = key[1:(end - 1)]
-function update_seed(dict)
-    if "seed" ∈ keys(dict)
-        dict["seed"] += 1
-    end
-    return dict
+# # Iterable
+# # There is an abstract Iterator supertype, each other can do something different
+# # Then, given a named tuple it will iterate over it all
+abstract type AbstractIterable end
+struct Iterable <: AbstractIterable
+    iterate
 end
-name(ex) = get(ex, "name", "optitest_" * format(now(), "yyyy-mm-dd:HM"))
-dir(ex) = get(ex, "dir", pwd())
-save_setup(ex) = get(ex, "save_setup", false)
-save_log(ex) = get(ex, "save_log", false)
-save_results(ex) = get(ex, "save_results", true)
-setup_file(ex) = joinpath(dir(ex), name(ex) * ".json")
-log_file(ex) = joinpath(dir(ex), name(ex) * ".log")
-results_file(ex) = joinpath(dir(ex), name(ex) * ".csv")
-
-function tests(experiment_dict::AbstractDict)
-    key_filter = get(experiment_dict, "key_filter", key!)
-    key_update = get(experiment_dict, "key_update", rm!)
-    special_fns = get(experiment_dict, "special_fns", update_seed)
-    pd = product_dict(
-        experiment_dict;
-        key_filter=key_filter,
-        key_update=key_update,
-        special_fns=special_fns,
+struct FlattenIterable <: AbstractIterable
+    iterate
+end
+struct Seed
+    seed::Integer
+    seed_ref::Ref{<:Integer}
+    Seed(seed) = new(seed, Ref(seed))
+end
+_iterate(any) = any
+_iterate(iter::AbstractIterable) = _iterate(getfield(iter, :iterate))
+_iterate(nt::Vector) = vcat(_iterate.(nt)...)
+function _iterate(nt::NamedTuple)
+    iterator_pairs = (
+        (k, v) for (k, v) in zip(keys(nt), values(nt)) if v isa AbstractIterable
     )
-    if isa(pd, AbstractDict)
-        return [pd]
+    if iterator_pairs == ()
+        return nt
     end
-    return pd
+    names, iterates = (first.(iterator_pairs), last.(iterator_pairs))
+    prods = Iterators.product(_iterate.(iterates)...)
+    iterates = (_copy_overwrite(nt, names, prod) for prod in prods)
+
+    # specials
+    seed_fn(k, v) = v isa Seed ? v.seed_ref[] += 1 : v
+    iterates = (_copy_apply(iter, seed_fn) for iter in iterates)
+
+    flatten = (k for (k, v) in iterator_pairs if v isa FlattenIterable)
+    iterates = (_copy_flatten(iter, flatten) for iter in iterates)
+
+    return collect(iterates)
 end
 
-# # redirect stdout and stderr
-function redirect_logs(func, log_file)
-    open(log_file, "w") do out
-        redirect_stdout(out) do
-            redirect_stderr(out) do
-                func()
-            end
-        end
+# # Experiment
+# TODO can I macro this?
+struct Experiment <: MaskedNamedTuple
+    _nt::NamedTuple
+    _mut::Dict{Symbol,Any}
+end
+Experiment(; kwargs...) = Experiment(NamedTuple{keys(kwargs)}(values(kwargs)))
+Experiment{names}(tuple::Tuple) where {names} = Experiment(NamedTuple{names}(tuple))
+
+# # Test
+struct Test <: MaskedNamedTuple
+    _nt::NamedTuple
+    _mut::Dict{Symbol,Any}
+end
+Test(nt::NamedTuple) = Test(nt, Dict{Symbol,Any}())
+Base.setproperty!(test::Test, sym::Symbol, val) = getfield(test, :_res)[sym] = val
+function Base.keys(test::Test)
+    return (keys(getfield(test, :_nt))..., keys(getfield(test, :_res))...)
+end
+Base.values(test::Test) = Tuple(getproperty(test, k) for k in keys(test))
+Base.NamedTuple(test::Test) = NamedTuple{keys(test)}(values(test))
+Base.Tuple(test::Test) = values(test)
+Base.show(io::IO, test::Test) = print(io, "Test", NamedTuple(test))
+function safe_get(test::Test, key)
+    try
+        return getproperty(test, key)
+    catch
+        return nothing
     end
 end
-
-function run(experiment::AbstractDict, solver::Function)::DataFrame
-    # shorthanding (im lazy)
-    ex = experiment
-
-    # save setup?
-    if save_setup(ex)
-        mkpath(dirname(setup_file(ex)))
-        open(setup_file(ex), "w") do io
-            print(io, ex, 4)
-        end
-    end
-
-    # capture output?
-    if save_log(ex)
-        results = redirect_logs(log_file(ex)) do
-            pmap(solver, tests(ex))
-        end
-    else
-        results = pmap(solver, tests(ex))
-    end
-
-    # create df
-    df = vcat(DataFrame.(flatten.(results))...)
-
-    # save df?
-    if save_results(ex)
-        write(results_file(ex), df)
-    end
-    return df
+function comma_line(test::Test)
+    return reduce(
+        (ln, key) -> ln * string(safe_get(test, key)) * ", ", keys(test); init=""
+    )[1:(end - 2)]
 end
